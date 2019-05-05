2e5e687b9858dad65b18ed38e0f31eae98debac2
chircancristi <cristianchircan1@gmail.com>
2019-05-05 14:17:30 +0300

    Removed sudo from config

--- tema3/config.sh
+++ tema3/config.sh
@@ -7,5 +7,5 @@ systemctl reload firewalld
 systemctl enable postfix
 systemctl start postfix 
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
-sudo EXTERNAL_URL="https://gitlab.example.com" yum install -y gitlab-ee
+EXTERNAL_URL="https://gitlab.example.com" yum install -y gitlab-ee
 
