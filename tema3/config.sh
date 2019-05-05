f212e85a914c04e7241608a6e4ad9ed7f5c6aab3
chircancristi <cristianchircan1@gmail.com>
2019-05-05 14:19:56 +0300

    No sudo left, I HOPE

--- tema3/config.sh
+++ tema3/config.sh
@@ -6,6 +6,6 @@ firewall-cmd --permanent --add-service=http
 systemctl reload firewalld
 systemctl enable postfix
 systemctl start postfix 
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh |  bash
 EXTERNAL_URL="https://gitlab.example.com" yum install -y gitlab-ee
 
